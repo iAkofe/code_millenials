5f042a26be012810bf4df1d22a9c27b3b1ed16e9
Toluwalope Olasupo <toluwalopeolasupo@gmail.com>
2023-03-07 20:40:10 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import styles from "./style";
-import { About, Button, Footer, FAQ, Features, Hero, Navbar, Newsletter, Pricing, Service, Team, Feat} from "./components";
+import styles from './style';
+import {
+  About,
+  Button,
+  Footer,
+  FAQ,
+  Features,
+  Hero,
+  Navbar,
+  Newsletter,
+  Pricing,
+  Service,
+  Team,
+  Feat,
+} from './components';
 
 const App = () => (
-  <div className="bg-primary w-full overflow-hidden">
+  <div className="bg-[transparent] w-full overflow-hidden">
     <div className={`bg-nav ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
@@ -14,21 +27,21 @@ const App = () => (
         <Hero />
       </div>
     </div>
-    
+
     <div className={`bg-primary`}>
-        <Service />
-        <div  className="w-full bg-secondary">
+      <Service />
+      <div className="w-full bg-secondary">
         <About />
-        </div>
-        <div className={`px-40 ${styles.flexCenter} py-[40px]`}>
+      </div>
+      <div className={`px-40 ${styles.flexCenter} py-[40px]`}>
         <Features />
-        </div>
-        <Feat />
-        <Team />
-        <FAQ />
-        <Pricing />
-        <Newsletter />
-        <Footer />
+      </div>
+      <Feat />
+      <Team />
+      <FAQ />
+
+      <Newsletter />
+      <Footer />
     </div>
   </div>
 );
